95d7eb9164fa0c8ae7f7aad5d8ae5197f81d4fc6
Marco Silvestri <m.silvestri03@gmail.com>
2020-04-16 17:20:48 +0200

    Add/Remove

--- main.js
+++ main.js
@@ -1,18 +1,57 @@
 $(document).ready(function () {
+
+    /* References */
+
+    var buttonSubmit = $('.app__input-area i');
+    var inputArea = $('.add-task');
+    var list = $('.todo');
+
+    $(buttonSubmit).click(addNewElement);
+
+    inputArea.keyup(function(e){
+        if (e.which == 13 || e.keyCode == 13){
+            addNewElement();
+        }
+    });
+
+    $('#app').on('click', '.todo li i', function(){
+        $(this).parent().remove();
+    });
+
+    /****************
+    *  Functions
+    *****************/
+
+    function addNewElement(){
+        var cloneTemplate = $('.ul-template li').clone();
+        cloneTemplate.prepend(inputArea.val().trim());
+        list.append(cloneTemplate);
+        inputArea.val('');
+    }
+
+    /****************
+    *  Debug AREA
+    *****************/
     
+    /* Data Source */
+/*
+    var dummyItems = [
+        'i am a dummy element',
+        'another dummy task',
+        'i am awesome, but still a dummy',
+        'click on this dummy',
+        'guy.brush dummy task'
+    ];
+*/
+    /* Populate */
+/*
+    for (let i = 0 ; i < dummyItems.length ; i++) {
+        var cloneTemplate = $('.ul-template li').clone();
 
+        cloneTemplate.prepend(dummyItems[i]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-    
+        list.append(cloneTemplate);
+        
+    };
+*/    
 });
