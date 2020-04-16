feb39c5c55a91cb19fe5a51c74c95dc2eddf14bc
Marco Silvestri <m.silvestri03@gmail.com>
2020-04-16 21:49:11 +0200

    with Calendar

--- main.js
+++ main.js
@@ -5,19 +5,39 @@ $(document).ready(function () {
     var buttonSubmit = $('.app__input-area i');
     var inputArea = $('.add-task');
     var list = $('.todo');
+    var dateDay = $('.calendar__day');
+    var dateName = $('.calendar__name');
+    var dateMonth = $('.calendar__month');
+    
+    var now = new Date();
+    var myDay;
+    var myMonth;
+    var myDate = now.getDate();
 
+    //Populate the calendar
+    getMonthAndDay(now);
+    dateDay.append(myDate);
+    dateName.append(myDay);
+    dateMonth.append(myMonth);
+    //var myFullDate = myDate + '-' + myDay + '-' + myMonth + '  -' ;
+
+
+    //Add the note on the click of the + button
     $(buttonSubmit).click(addNewElement);
 
+    // Add the new note on the hit of ENTER
     inputArea.keyup(function(e){
-        if (e.which == 13 || e.keyCode == 13){
+        if (e.which == 13 || e.keyCode == 13){//13 is ENTER
             addNewElement();
         }
     });
 
-    $('#app').on('click', '.todo li i', function(){
+    //Remove a note when clicking on the - icon
+    $('#app').on('click', '.todo li span', function(){
         $(this).parent().remove();
     });
 
+    //Add a line-through on the click of the note
     $('#app').on('click', '.todo li', function(){
         $(this).toggleClass('stroked');
     });
@@ -33,11 +53,77 @@ $(document).ready(function () {
         }
         else {
             var cloneTemplate = $('.ul-template li').clone();
-            cloneTemplate.prepend(inputArea.val().trim());
+            cloneTemplate.prepend(/*myFullDate,*/ inputArea.val().trim());
             list.append(cloneTemplate);
             inputArea.val('');
         }
     }
+
+    //Get a user friendly month and day
+    function getMonthAndDay(actualDate){
+        switch (actualDate.getDay()) {
+            case 0:
+                myDay = 'Sun';
+                break;
+            case 1:
+                myDay = 'Mon';
+                break;
+            case 2:
+                myDay = 'Tue';
+                break;
+            case 3:
+                myDay = 'Wed';
+                break;
+            case 4:
+                myDay = 'Thu';
+                break;
+            case 5:
+                myDay = 'Fri';
+                break;
+            case 6:
+                myDay = 'Sat';
+                break;
+        }
+        
+        switch (actualDate.getMonth()) {
+            case 0:
+                myMonth = 'January';
+                break;
+            case 1:
+                myMonth = 'February';
+                break;
+            case 2:
+                myMonth = 'March';
+                break;
+            case 3:
+                myMonth = 'April';
+                break;
+            case 4:
+                myMonth = 'May';
+                break;
+            case 5:
+                myMonth = 'June';
+                break;
+            case 6:
+                myMonth = 'July';
+                break;
+            case 7:
+                myMonth = 'August';
+                break;
+            case 8:
+                myMonth = 'September';
+                break;
+            case 9:
+                myMonth = 'October';
+                break;
+            case 10:
+                myMonth = 'November';;
+                break;
+            case 11:
+                myMonth = 'December';
+                break;
+        }
+};
 
     /****************
     *  Debug AREA
